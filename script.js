606c8d0e3562342ea5b7138df536089e2e049b87
CodeVortex <codevortex001@gmail.com>
2025-10-09 01:21:21 +0100

    Added Tab Component

--- script.js
+++ script.js
@@ -158,3 +158,25 @@ h1.lastElementChild.style.color = 'white';
 
 console.log(h1.parentNode);
 h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+
+  //Handling null clicks
+  if (!clicked) return;
+
+  //Active tab
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+
+  //Activate Content Area
+  document
+    .querySelector(`.operations__content--${click.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
